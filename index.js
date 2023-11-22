e9abf0423031063ad5e9438bd9c8766a1ff83b91
Kingson.Arum <nwajiudo@gmail.com>
2023-11-22 12:53:49 +0100

    commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 async function printAdvice() {
   const advice = document.querySelector("#adviceText");
-  const adviceId = document.querySelector("#adviceId");
+  const adviceId = document.querySelector(".adviceId2");
 
   const response = await fetch("https://api.adviceslip.com/advice");
   const word = await response.json();
-  advice.innerHTML = word.slip.advice;
+  advice.innerHTML =  word.slip.advice;
+  adviceId.innerHTML = word.slip.id;
   console.log(word.slip.advice);
 }
